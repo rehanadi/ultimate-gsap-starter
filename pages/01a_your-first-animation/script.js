9a413453957d68db6b5bb26ee06eac471f8bc9ba
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2025-10-29 08:39:22 +0700

    feat: first animation

---
+++ pages/01a_your-first-animation/script.js
@@ -0,0 +1,14 @@
+gsap.to(".card", {
+  opacity: 1,
+  scale: 1,
+  duration: 2,
+  onComplete: () => {
+    gsap.to(".card", {
+      boxShadow: "0 20px 80px rgba(29, 209, 161, 0.6)", // glow effect
+      y: -20, // move up by 20 pixels
+      repeat: -1, // infinite
+      yoyo: true, // reverse on each repeat
+      duration: 0.5
+    });
+  }
+});
